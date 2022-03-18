402762a915e729d91cdf1fede11df2508b25a988
DamVanManh <hnammad7@gmail.com>
2022-03-18 16:45:59 +0700

    remove log

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,17 +1,28 @@
-const request = require('request')
+const request = require("request");
 
 const forecast = (latitude, longitude, callback) => {
-    const url = 'https://api.darksky.net/forecast/9d1465c6f3bb7a6c71944bdd8548d026/' + latitude + ',' + longitude
+  const url =
+    "http://api.weatherstack.com/current?access_key=2e5c4f61cf21c58b9f2866ad19af1c3b&query=" +
+    latitude +
+    "," +
+    longitude;
+  request({ url, json: true }, (error, { body }) => {
+    if (error) {
+      callback("Unable to connect to weather service!", undefined);
+    } else if (body.error) {
+      callback("Unable to find location", undefined);
+    } else {
+      callback(
+        undefined,
+        body.location.localtime +
+          " It is currently " +
+          body.current.temperature +
+          " degress out. There is a " +
+          body.current.humidity +
+          "% chance of rain."
+      );
+    }
+  });
+};
 
-    request({ url, json: true }, (error, { body }) => {
-        if (error) {
-            callback('Unable to connect to weather service!', undefined)
-        } else if (body.error) {
-            callback('Unable to find location', undefined)
-        } else {
-            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degress out. There is a ' + body.currently.precipProbability + '% chance of rain.')
-        }
-    })
-}
-
-module.exports = forecast
+module.exports = forecast;
